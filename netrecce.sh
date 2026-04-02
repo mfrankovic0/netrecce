c7306b81ba42c8bbd4984a321b5d14263c56631b
Michael <37089637+mfrankovic0@users.noreply.github.com>
2026-04-02 16:18:02 -0400

    Update netrecce.sh

--- netrecce.sh
+++ netrecce.sh
@@ -4,19 +4,42 @@ ip_regex="^([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])[.]\
 ([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])[.]\
 ([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])[.]\
 ([0-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])$"
+# probably a better way to write this
+
 
 input () {
-    while true; do
+    #recieve user input as a IP address or hostname and store to a variable
+    while true; do	
         read -p "Please enter an IP address or hostname: " host
             if [[ $host =~ $ip_regex ]]; then
-                break
-            elif [[ -n $host ]]; then
-                break;
-            else
-                echo "Not valid IP address or hostname."
-            fi
+	        break 
+            elif [[ -n $host ]]; then 
+		break;
+	    else
+     		echo "Not valid IP address or hostname."
+    	    fi
     done
     echo "Host selected: $host"
 }
 
-input
+ping_test () {
+    #intial ping to test host is reachable
+    if ping -c 1 $host > /dev/null 2>&1; then
+	echo "Host reached."
+        return 0
+    else
+	echo "Host not reachable or invalid."
+	return 1
+    fi
+}
+
+main () {
+    while true; do
+        input
+        if ping_test; then
+            break
+        fi  
+    done
+}
+
+main
